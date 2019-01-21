6d19e8081647d551bd01d39e23c1f5036a0ddda6
Your Name <you@example.com>
2019-01-21 11:23:17 -0500

    Have no grasp of classes.

--- bship.rb
+++ bship.rb
@@ -7,19 +7,29 @@ class Grid12
         @wcol = wcol
     end
 
+        print "OWN BOARD\n"
+        print "--|\s1|\s2|\s3|\s4|\s5|\s6|\s7|\s8|\s9|10|11|12|\n"
+        print "\s1|\n\s2|\n\s3|\n\s4|\n\s5|\n\s6|\n\s7|\n\s8|\n\s9|\n10|\n11|\n12|\n---------------------------------------\n"
+        print "\n\n"
+
+        print "ENEMY BOARD\n"
+        print "--|\s1|\s2|\s3|\s4|\s5|\s6|\s7|\s8|\s9|10|11|12|\n"
+        print "\s1|\n\s2|\n\s3|\n\s4|\n\s5|\n\s6|\n\s7|\n\s8|\n\s9|\n10|\n11|\n12|\n---------------------------------------\n"
+        print "\n\n"
+
     @row = 1
     @col = 1
-    print "OWN BOARD\n"
-    print "--|\s1|\s2|\s3|\s4|\s5|\s6|\s7|\s8|\s9|10|11|12|\n"
-    print "\s1|\n\s2|\n\s3|\n\s4|\n\s5|\n\s6|\n\s7|\n\s8|\n\s9|\n10|\n11|\n12|\n---------------------------------------\n"
-    print "\n\n"
-
-    print "ENEMY BOARD\n"
-    print "--|\s1|\s2|\s3|\s4|\s5|\s6|\s7|\s8|\s9|10|11|12|\n"
-    print "\s1|\n\s2|\n\s3|\n\s4|\n\s5|\n\s6|\n\s7|\n\s8|\n\s9|\n10|\n11|\n12|\n---------------------------------------\n"
-    print "\n\n"
-
-
+    while @row <= 12
+        @col = 1
+        while @col < 12
+            print " ~"
+            @col += 1
+            if @col == 12
+                print " ~\n"
+                @row += 1
+            end
+        end
+    end
 end
 
 
@@ -29,24 +39,6 @@ end
 
 
 
-# @rows12 = 12
-# @cols12 = 12
-
-# puts "We have #{@rows12} rows and #{@cols12} columns."
-
-# class BoardTwoFour
-#     def initialize(rows, columns)
-#         @rows24 = rows
-#         @cols24 = columns
-#     end
-# end
-
-# class BoardThreeSix
-#     def initialize(rows, columns)
-#         @rows36 = rows
-#         @cols36 = columns
-#     end
-# end
 
 
 
