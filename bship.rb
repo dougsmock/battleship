8c65f3b99dbf20280ca47614406fe1ab390dafac
Your Name <you@example.com>
2019-01-21 09:44:38 -0500

    Rows, column headings set.

--- bship.rb
+++ bship.rb
@@ -9,14 +9,15 @@ class Grid12
 
     @row = 1
     @col = 1
-    while @row <= 12
-            print "#{@row}|"
-            @row += 1
-        while @col <= 12
-            puts "#{@col}|"
-            @col += 1
-        end
-    end
+    print "OWN BOARD\n"
+    print "--|\s1|\s2|\s3|\s4|\s5|\s6|\s7|\s8|\s9|10|11|12|\n"
+    print "\s1|\n\s2|\n\s3|\n\s4|\n\s5|\n\s6|\n\s7|\n\s8|\n\s9|\n10|\n11|\n12|\n---------------------------------------\n"
+    print "\n\n"
+
+    print "ENEMY BOARD\n"
+    print "--|\s1|\s2|\s3|\s4|\s5|\s6|\s7|\s8|\s9|10|11|12|\n"
+    print "\s1|\n\s2|\n\s3|\n\s4|\n\s5|\n\s6|\n\s7|\n\s8|\n\s9|\n10|\n11|\n12|\n---------------------------------------\n"
+    print "\n\n"
 
 
 end
