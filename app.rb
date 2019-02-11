158aef23361d0a92be866c23701a94c3206551e2
Your Name <you@example.com>
2019-02-11 15:29:02 -0500

    Solved app.rb paths to individual grids.

--- app.rb
+++ app.rb
@@ -12,19 +12,27 @@ get '/' do
     erb :rules
 end
 
-post '/rules' do
-    redirect '/choose'
+post '/size1' do
+    redirect '/size1'
 end
 
-get '/choose' do
+post '/size2' do
+    redirect '/size2'
+end
+
+post '/size3' do
+    redirect '/size3'
+end
+
+get '/size1' do
     erb :board12
 end
 
-get '/choose' do
+get '/size2' do
     erb :board24
 end
 
-get '/choose' do
+get '/size3' do
     erb :board36
 end
 
@@ -33,23 +41,4 @@ end
 
 
 
-# get '/checkout' do
-#     # session[:crust]
-#     erb :checkout, locals:{type: session[:type], size: session[:size], crust: session[:crust], toppings1: session[:toppings1], subtotal: session[:subtotal], sales_tax: session[:sales_tax], grand_total: session[:grand_total]}
-#   end
-  
-#   post '/checkout' do
-#     puts "Hello World"
-#     puts "#{params} is params in post '/lastpage' do"
-#     session[:name] = params[:name]
-#     session[:address] = params[:address]
-#     session[:phone] = params[:phone]
-#     session[:grand_total] = params[:grand_tota"l]
-#     session[:toppings1] = params[:toppings1]
-#     session[:crust] = params[:crust]
-#     session[:type] = params[:type]
-#     session[:size] = params[:size]
-#     p session
-#     redirect '/lastpage'
-#   end
 
